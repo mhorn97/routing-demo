2aac34d4539f0035b1c3812396d33186ff03f4c1
Michael Horn <michaelhorn1@hotmail.com>
2018-01-12 14:50:43 -0500

    added routes

--- index.php
+++ index.php
@@ -2,10 +2,27 @@
 
 require_once ('vendor/autoload.php');
 
+
+
 $f3 = Base::instance();
+
+//Set debug level
+$f3->set('DEBUG', 3);
 
 $f3 -> route('GET /', function() {
     echo "<h1>Routing Demo</h1>";
+}
+);
+
+//Define a page1 route
+$f3 -> route('GET /page1', function() {
+    echo "<h1>This is page1</h1>";
+}
+);
+
+//Define a page2 route
+$f3 -> route('GET /page2', function() {
+    echo "<h1>This is page 2</h1>";
 }
 );
 
